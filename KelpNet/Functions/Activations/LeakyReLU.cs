eeb60b1c25c896399c54d2d9357dbc34a374fa87
harujoh <harujoh.yuki@gmail.com>
2016-07-29 17:00:10 +0900

    LeakyReLUを追加

---
+++ KelpNet/Functions/Activations/LeakyReLU.cs
@@ -0,0 +1,41 @@
+﻿namespace KelpNet.Functions.Activations
+{
+    public class LeakyReLU : Function, IPredictableFunction
+    {
+        private readonly double slope;
+
+        public LeakyReLU(double slope = 0.2)
+        {
+            this.slope = slope;
+        }
+
+        public override NdArray Forward(NdArray x)
+        {
+            PrevOutput = new NdArray(x);
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (PrevOutput.Data[i] < 0) PrevOutput.Data[i] *= this.slope;
+            }
+
+            return PrevOutput;
+        }
+
+        public override NdArray Backward(NdArray gy)
+        {
+            NdArray result = new NdArray(gy);
+
+            for (int i = 0; i < gy.Length; i++)
+            {
+                if (PrevOutput.Data[i] < 0) PrevOutput.Data[i] *= this.slope;
+            }
+
+            return result;
+        }
+
+        public NdArray Predict(NdArray input)
+        {
+            return Forward(input);
+        }
+    }
+}
