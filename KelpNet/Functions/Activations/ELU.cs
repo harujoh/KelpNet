6378ab9a079dc0d865c89684c3f365d0873c7183
harujoh <harujoh.yuki@gmail.com>
2016-09-27 13:50:38 +0900

    ELUを追加

---
+++ KelpNet/Functions/Activations/ELU.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace KelpNet.Functions.Activations
+{
+    public class ELU : NeedPreviousDataFunction
+    {
+        private readonly double _alpha;
+
+        public ELU(double alpha, string name = "ELU") : base(name)
+        {
+            this._alpha = alpha;
+        }
+
+        protected override NdArray NeedPreviousForward(NdArray x)
+        {
+            NdArray y = NdArray.EmptyLike(x);
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                y.Data[i] = x.Data[i] >= 0 ? x.Data[i] : this._alpha * (Math.Exp(x.Data[i]) - 1);
+            }
+
+            return y;
+        }
+
+        protected override NdArray NeedPreviousBackward(NdArray gy, NdArray prevInput, NdArray prevOutput)
+        {
+            NdArray result = NdArray.EmptyLike(gy);
+
+            for (int i = 0; i < gy.Length; i++)
+            {
+                result.Data[i] = prevOutput.Data[i] >= 0 ? gy.Data[i] : gy.Data[i] * this._alpha * Math.Exp(prevInput.Data[i]);
+            }
+
+            return result;
+        }
+    }
+}
