f4da3a7a6459a86779b8b438dce98bf071bbcf76
harujoh <harujoh.yuki@gmail.com>
2016-10-31 09:53:42 +0900

    Test12のDNI層をLabelを含んで学習するcDNIに変更

--- KelpNetTester/Tests/Test12.cs
+++ KelpNetTester/Tests/Test12.cs
@@ -12,6 +12,8 @@ using KelpNet.Optimizers;
 namespace KelpNetTester.Tests
 {
     //Decoupled Neural Interfaces using Synthetic GradientsによるMNIST（手書き文字）の学習
+    // 教師信号にラベル情報を混ぜ込むcDNI
+    // モデルDと表現されている全層のDecoupledを非同期で実行
     // http://ralo23.hatenablog.com/entry/2016/10/22/233405
     class Test12
     {
@@ -25,12 +27,38 @@ namespace KelpNetTester.Tests
         //性能評価時のデータ数
         const int TEST_DATA_COUNT = 100;
 
+        class ResultDataSet
+        {
+            public NdArray[] Result;
+            public int[][] Label;
+
+            public ResultDataSet(NdArray[] result, int[][] label)
+            {
+                this.Result = result;
+                this.Label = label;
+            }
+
+            public double[][] GetTrainData()
+            {
+                //第一層の傾きを取得
+                var train = new double[BATCH_DATA_COUNT][];
+
+                for (int k = 0; k < BATCH_DATA_COUNT; k++)
+                {
+                    train[k] = new double[256 + 10];
+                    train[k][256 + this.Label[k][0]] = 1.0;
+                    Buffer.BlockCopy(this.Result[k].Data, 0, train[k], 0, sizeof(double) * 256);
+                }
+
+                return train;
+            }
+        }
+
         public static void Run()
         {
             //MNISTのデータを用意する
             Console.WriteLine("MNIST Data Loading...");
             MnistData mnistData = new MnistData();
-
 
             Console.WriteLine("Training Start...");
 
@@ -66,34 +94,34 @@ namespace KelpNetTester.Tests
                 Layer4
             );
 
-            FunctionStack DNI1 = new FunctionStack(
-                new Linear(256, 1024, name: "DNI1 Linear1"),
-                new BatchNormalization(1024, name: "DNI1 Nrom1"),
-                new ReLU(name: "DNI1 ReLU1"),
-                new Linear(1024, 1024, name: "DNI1 Linear2"),
-                new BatchNormalization(1024, name: "DNI1 Nrom2"),
-                new ReLU(name: "DNI1 ReLU2"),
+            FunctionStack cDNI1 = new FunctionStack(
+                new Linear(256 + 10, 1024, name: "cDNI1 Linear1"),
+                new BatchNormalization(1024, name: "cDNI1 Nrom1"),
+                new ReLU(name: "cDNI1 ReLU1"),
+                new Linear(1024, 1024, name: "cDNI1 Linear2"),
+                new BatchNormalization(1024, name: "cDNI1 Nrom2"),
+                new ReLU(name: "cDNI1 ReLU2"),
                 new Linear(1024, 256, initialW: new double[1024, 256], name: "DNI1 Linear3")
             );
 
-            FunctionStack DNI2 = new FunctionStack(
-                new Linear(256, 1024, name: "DNI2 Linear1"),
-                new BatchNormalization(1024, name: "DNI2 Nrom1"),
-                new ReLU(name: "DNI2 ReLU1"),
-                new Linear(1024, 1024, name: "DNI2 Linear2"),
-                new BatchNormalization(1024, name: "DNI2 Nrom2"),
-                new ReLU(name: "DNI2 ReLU2"),
-                new Linear(1024, 256, initialW: new double[1024, 256], name: "DNI2 Linear3")
+            FunctionStack cDNI2 = new FunctionStack(
+                new Linear(256, 1024, name: "cDNI2 Linear1"),
+                new BatchNormalization(1024, name: "cDNI2 Nrom1"),
+                new ReLU(name: "cDNI2 ReLU1"),
+                new Linear(1024, 1024, name: "cDNI2 Linear2"),
+                new BatchNormalization(1024, name: "cDNI2 Nrom2"),
+                new ReLU(name: "cDNI2 ReLU2"),
+                new Linear(1024, 256, initialW: new double[1024, 256], name: "cDNI2 Linear3")
             );
 
-            FunctionStack DNI3 = new FunctionStack(
-                new Linear(256, 1024, name: "DNI3 Linear1"),
-                new BatchNormalization(1024, name: "DNI3 Nrom1"),
-                new ReLU(name: "DNI3 ReLU1"),
-                new Linear(1024, 1024, name: "DNI3 Linear2"),
-                new BatchNormalization(1024, name: "DNI3 Nrom2"),
-                new ReLU(name: "DNI3 ReLU2"),
-                new Linear(1024, 256, initialW: new double[1024, 256], name: "DNI3 Linear3")
+            FunctionStack cDNI3 = new FunctionStack(
+                new Linear(256, 1024, name: "cDNI3 Linear1"),
+                new BatchNormalization(1024, name: "cDNI3 Nrom1"),
+                new ReLU(name: "cDNI3 ReLU1"),
+                new Linear(1024, 1024, name: "cDNI3 Linear2"),
+                new BatchNormalization(1024, name: "cDNI3 Nrom2"),
+                new ReLU(name: "cDNI3 ReLU2"),
+                new Linear(1024, 256, initialW: new double[1024, 256], name: "cDNI3 Linear3")
             );
 
             //optimizerを宣言
@@ -101,9 +129,10 @@ namespace KelpNetTester.Tests
             Layer2.SetOptimizer(new Adam());
             Layer3.SetOptimizer(new Adam());
             Layer4.SetOptimizer(new Adam());
-            DNI1.SetOptimizer(new Adam());
-            DNI2.SetOptimizer(new Adam());
-            DNI3.SetOptimizer(new Adam());
+
+            cDNI1.SetOptimizer(new Adam());
+            cDNI2.SetOptimizer(new Adam());
+            cDNI3.SetOptimizer(new Adam());
 
 
             //三世代学習
@@ -114,25 +143,24 @@ namespace KelpNetTester.Tests
                 //全体での誤差を集計
                 List<double> totalLoss = new List<double>();
 
-                List<double> DNI1totalLoss = new List<double>();
+                List<double> cDNI1totalLoss = new List<double>();
 
-                List<double> DNI2totalLoss = new List<double>();
+                List<double> cDNI2totalLoss = new List<double>();
 
-                List<double> DNI3totalLoss = new List<double>();
+                List<double> cDNI3totalLoss = new List<double>();
 
 
-                var layer1ForwardResult = new List<NdArray[]>();
-                var layer2ForwardResult = new List<NdArray[]>();
-                var layer3ForwardResult = new List<NdArray[]>();
+                var layer1ForwardResults = new List<ResultDataSet>();
+                var layer2ForwardResults = new List<ResultDataSet>();
+                var layer3ForwardResults = new List<ResultDataSet>();
 
                 var layer2BackwardResult = new List<NdArray[]>();
                 var layer3BackwardResult = new List<NdArray[]>();
                 var layer4BackwardResult = new List<NdArray[]>();
 
-                var DNI1Result = new List<NdArray[]>();
-                var DNI2Result = new List<NdArray[]>();
-                var DNI3Result = new List<NdArray[]>();
-                var datasetXResult = new List<int[][]>();
+                var cDNI1Result = new List<NdArray[]>();
+                var cDNI2Result = new List<NdArray[]>();
+                var cDNI3Result = new List<NdArray[]>();
 
                 //何回バッチを実行するか
                 for (int i = 1; i < TRAIN_DATA_COUNT + 1; i++)
@@ -144,100 +172,96 @@ namespace KelpNetTester.Tests
                         datasetX[j] = mnistData.GetRandomXSet(BATCH_DATA_COUNT);
                     }
 
-                    //第一層を3～10回実行
+                    //第一層を2～6回実行
                     var count = Mother.Dice.Next(2, 6);
                     for (int j = 0; j < count; j++)
                     {
-                        layer1ForwardResult.Add(Layer1.Forward(datasetX[j].Data));
-                        datasetXResult.Add(datasetX[j].Label);
+                        var layer1ForwardResult = Layer1.Forward(datasetX[j].Data);
+                        var layer1ResultDataSet = new ResultDataSet(layer1ForwardResult, datasetX[j].Label);
+                        layer1ForwardResults.Add(layer1ResultDataSet);
 
-                        DNI1Result.Add(DNI1.Forward(layer1ForwardResult.Last()));
+                        ////第一層の傾きを取得
+                        cDNI1Result.Add(cDNI1.Forward(layer1ResultDataSet.GetTrainData()));
 
-                        //第一層の傾きを取得
-                        if (0.5 < Mother.Dice.NextDouble())
-                        {
-                            //第一層を更新
-                            Layer1.Backward(DNI1Result.Last());
-                            Layer1.Update();
-                        }
+                        //第一層を更新
+                        Layer1.Backward(cDNI1Result.Last());
+                        Layer1.Update();
                     }
 
-                    //第二層を3～10回実行
+                    //第二層を2～6回実行
                     count = Mother.Dice.Next(2, 6);
-                    for (int j = 0; j < count && layer1ForwardResult.Count > 0; j++)
+                    for (int j = 0; j < count && layer1ForwardResults.Count > 0; j++)
                     {
-                        layer2ForwardResult.Add(Layer2.Forward(layer1ForwardResult[0]));
-                        layer1ForwardResult.RemoveAt(0);
-
-                        DNI2Result.Add(DNI2.Forward(layer2ForwardResult.Last()));
+                        var layer2ForwardResult = Layer2.Forward(layer1ForwardResults[0].Result);
+                        var layer2ResultDataSet = new ResultDataSet(layer2ForwardResult, layer1ForwardResults[0].Label);
+                        layer2ForwardResults.Add(layer2ResultDataSet);
+                        layer1ForwardResults.RemoveAt(0);
 
                         //第二層の傾きを取得
-                        if (0.5 < Mother.Dice.NextDouble())
-                        {
-                            //第二層を更新
-                            layer2BackwardResult.Add(Layer2.Backward(DNI2Result.Last()));
-                            Layer2.Update();
-                        }
+                        cDNI2Result.Add(cDNI2.Forward(layer2ResultDataSet.GetTrainData()));
+
+                        //第二層を更新
+                        layer2BackwardResult.Add(Layer2.Backward(cDNI2Result.Last()));
+                        Layer2.Update();
                     }
 
-                    //第一層用のDNIの学習を3～10回実行
-                    double DNI1loss = 0;
+                    //第一層用のcDNIの学習を2～6回実行
+                    double cDNI1loss = 0;
                     count = Mother.Dice.Next(2, 6);
-                    for (int j = 0; j < count && layer2BackwardResult.Count > 0 && DNI1Result.Count > 0; j++)
+                    for (int j = 0; j < count && layer2BackwardResult.Count > 0 && cDNI1Result.Count > 0; j++)
                     {
-                        var DNI1lossResult = LossFunctions.MeanSquaredError(DNI1Result[0], layer2BackwardResult[0], out DNI1loss);
-                        DNI1Result.RemoveAt(0);
+                        var DNI1lossResult = LossFunctions.MeanSquaredError(cDNI1Result[0], layer2BackwardResult[0], out cDNI1loss);
+                        cDNI1Result.RemoveAt(0);
                         layer2BackwardResult.RemoveAt(0);
 
-                        DNI1.Backward(DNI1lossResult);
-                        DNI1.Update();
-                        DNI1totalLoss.Add(DNI1loss);
+                        cDNI1.Backward(DNI1lossResult);
+                        cDNI1.Update();
+                        cDNI1totalLoss.Add(cDNI1loss);
                     }
 
-                    //第三層を3～10回実行
+                    //第三層を2～6回実行
                     count = Mother.Dice.Next(2, 6);
-                    for (int j = 0; j < count && layer2ForwardResult.Count > 0; j++)
+                    for (int j = 0; j < count && layer2ForwardResults.Count > 0; j++)
                     {
-                        layer3ForwardResult.Add(Layer3.Forward(layer2ForwardResult[0]));
-                        layer2ForwardResult.RemoveAt(0);
-
-                        DNI3Result.Add(DNI3.Forward(layer3ForwardResult.Last()));
+                        var layer3ForwardResult = Layer3.Forward(layer2ForwardResults[0].Result);
+                        var layer3ResultDataSet = new ResultDataSet(layer3ForwardResult, layer2ForwardResults[0].Label);
+                        layer3ForwardResults.Add(layer3ResultDataSet);
+                        layer2ForwardResults.RemoveAt(0);
 
                         //第三層の傾きを取得
-                        if (0.5 < Mother.Dice.NextDouble())
-                        {
-                            //第三層を更新
-                            layer3BackwardResult.Add(Layer3.Backward(DNI3Result.Last()));
-                            Layer3.Update();
-                        }
+                        cDNI3Result.Add(cDNI3.Forward(layer3ResultDataSet.GetTrainData()));
+
+                        //第三層を更新
+                        layer3BackwardResult.Add(Layer3.Backward(cDNI3Result.Last()));
+                        Layer3.Update();
                     }
 
 
-                    //第二層用のDNIの学習を3～10回実行
-                    double DNI2loss = 0;
+                    //第二層用のcDNIの学習を2～6回実行
+                    double cDNI2loss = 0;
                     count = Mother.Dice.Next(2, 6);
-                    for (int j = 0; j < count && layer3BackwardResult.Count > 0 && DNI2Result.Count > 0; j++)
+                    for (int j = 0; j < count && layer3BackwardResult.Count > 0 && cDNI2Result.Count > 0; j++)
                     {
-                        var DNI2lossResult = LossFunctions.MeanSquaredError(DNI2Result[0], layer3BackwardResult[0], out DNI2loss);
-                        DNI2Result.RemoveAt(0);
+                        var DNI2lossResult = LossFunctions.MeanSquaredError(cDNI2Result[0], layer3BackwardResult[0], out cDNI2loss);
+                        cDNI2Result.RemoveAt(0);
                         layer3BackwardResult.RemoveAt(0);
 
-                        DNI2.Backward(DNI2lossResult);
-                        DNI2.Update();
-                        DNI2totalLoss.Add(DNI2loss);
+                        cDNI2.Backward(DNI2lossResult);
+                        cDNI2.Update();
+                        cDNI2totalLoss.Add(cDNI2loss);
                     }
 
 
                     double sumLoss = 0;
-                    //第四層を3～10回実行
+                    //第四層を2～6回実行
                     count = Mother.Dice.Next(2, 6);
-                    for (int j = 0; j < count && layer3ForwardResult.Count > 0 && datasetXResult.Count > 0; j++)
+                    for (int j = 0; j < count && layer3ForwardResults.Count > 0; j++)
                     {
-                        var layer4ForwardResult = Layer4.Forward(layer3ForwardResult[0]);
+                        var layer4ForwardResult = Layer4.Forward(layer3ForwardResults[0].Result);
 
                         //第四層の傾きを取得
-                        var lossResult = LossFunctions.SoftmaxCrossEntropy(layer4ForwardResult, datasetXResult[0], out sumLoss);
-                        datasetXResult.RemoveAt(0);
+                        var lossResult = LossFunctions.SoftmaxCrossEntropy(layer4ForwardResult, layer3ForwardResults[0].Label, out sumLoss);
+                        layer3ForwardResults.RemoveAt(0);
 
                         //第四層を更新
                         layer4BackwardResult.Add(Layer4.Backward(lossResult));
@@ -245,18 +269,18 @@ namespace KelpNetTester.Tests
                         totalLoss.Add(sumLoss);
                     }
 
-                    //第三層用のDNIの学習を実行
-                    double DNI3loss = 0;
+                    //第三層用のcDNIの学習を実行
+                    double cDNI3loss = 0;
                     count = Mother.Dice.Next(2, 6);
-                    for (int j = 0; j < count && layer4BackwardResult.Count > 0 && DNI3Result.Count > 0; j++)
+                    for (int j = 0; j < count && layer4BackwardResult.Count > 0 && cDNI3Result.Count > 0; j++)
                     {
-                        var DNI3lossResult = LossFunctions.MeanSquaredError(DNI3Result[0], layer4BackwardResult[0], out DNI3loss);
-                        DNI3Result.RemoveAt(0);
+                        var DNI3lossResult = LossFunctions.MeanSquaredError(cDNI3Result[0], layer4BackwardResult[0], out cDNI3loss);
+                        cDNI3Result.RemoveAt(0);
                         layer4BackwardResult.RemoveAt(0);
 
-                        DNI3.Backward(DNI3lossResult);
-                        DNI3.Update();
-                        DNI3totalLoss.Add(DNI3loss);
+                        cDNI3.Backward(DNI3lossResult);
+                        cDNI3.Update();
+                        cDNI3totalLoss.Add(cDNI3loss);
                     }
 
                     Console.WriteLine("\nbatch count " + i + "/" + TRAIN_DATA_COUNT);
@@ -264,13 +288,13 @@ namespace KelpNetTester.Tests
                     Console.WriteLine("total loss " + totalLoss.Average());
                     Console.WriteLine("local loss " + sumLoss);
 
-                    Console.WriteLine("\nDNI1 total loss " + DNI1totalLoss.Average());
-                    Console.WriteLine("DNI2 total loss " + DNI2totalLoss.Average());
-                    Console.WriteLine("DNI3 total loss " + DNI3totalLoss.Average());
+                    Console.WriteLine("\ncDNI1 total loss " + cDNI1totalLoss.Average());
+                    Console.WriteLine("cDNI2 total loss " + cDNI2totalLoss.Average());
+                    Console.WriteLine("cDNI3 total loss " + cDNI3totalLoss.Average());
 
-                    Console.WriteLine("\nDNI1 local loss " + DNI1loss);
-                    Console.WriteLine("DNI2 local loss " + DNI2loss);
-                    Console.WriteLine("DNI3 local loss " + DNI3loss);
+                    Console.WriteLine("\ncDNI1 local loss " + cDNI1loss);
+                    Console.WriteLine("cDNI2 local loss " + cDNI2loss);
+                    Console.WriteLine("cDNI3 local loss " + cDNI3loss);
 
                     //20回バッチを動かしたら精度をテストする
                     if (i % 20 == 0)
