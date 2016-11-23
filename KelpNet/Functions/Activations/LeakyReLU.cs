74545ad091dfb028a3fa437c8c2309bf2649137f
harujoh <harujoh.yuki@gmail.com>
2016-11-23 17:09:39 +0900

    LeakyReLUのバグを修正

--- KelpNet/Functions/Activations/LeakyReLU.cs
+++ KelpNet/Functions/Activations/LeakyReLU.cs
@@ -19,7 +19,7 @@ namespace KelpNet.Functions.Activations
 
             for (int i = 0; i < x.Length; i++)
             {
-                if (y[i] < 0) y[i] *= this._slope;
+                y[i] = x.Data[i] < 0 ? x.Data[i] *= this._slope : x.Data[i];
             }
 
             return new NdArray(y, x.Shape);
