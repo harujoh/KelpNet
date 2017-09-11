9a410b6381789ec8125f953cdefbc21c055f391b
harujoh <harujoh.yuki@gmail.com>
2017-09-11 10:36:45 +0900

    パラメータの初期化が正常に動作しないナグを修正

--- KelpNet/Common/Functions/FunctionParameter.cs
+++ KelpNet/Common/Functions/FunctionParameter.cs
@@ -36,7 +36,7 @@ namespace KelpNet.Common.Functions
         public void ClearGrad()
         {
             //0埋め
-            this.Grad.Clear();
+            this.Grad.Fill(0);
 
             //カウンタをリセット
             this.TrainCount = 0;
