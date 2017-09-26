fe3b4bc948060b72984e30c3e833f233a71629fd
harujoh <harujoh.yuki@gmail.com>
2017-09-26 13:45:59 +0900

    Slopeが0のときReLUを作成するように変更

--- CaffemodelLoader/CaffemodelDataLoader.cs
+++ CaffemodelLoader/CaffemodelDataLoader.cs
@@ -70,7 +70,7 @@ namespace CaffemodelLoader
                     return SetupPooling(layer.PoolingParam, layer.Name);
 
                 case "ReLU":
-                    return layer.ReluParam != null ? new LeakyReLU(layer.ReluParam.NegativeSlope, layer.Name) : new LeakyReLU(name: layer.Name);
+                    return layer.ReluParam != null ? layer.ReluParam.NegativeSlope == 0 ? (Function)new ReLU(layer.Name) : (Function)new LeakyReLU(layer.ReluParam.NegativeSlope, layer.Name) : (Function)new LeakyReLU(name: layer.Name);
 
                 case "InnerProduct":
                     return SetupInnerProduct(layer.InnerProductParam, layer.Blobs, layer.Name);
