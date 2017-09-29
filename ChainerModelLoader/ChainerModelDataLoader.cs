e4c2003ed12e8eda4b966bc378663cdc3d211e8d
harujoh <harujoh.yuki@gmail.com>
2017-09-29 18:58:16 +0900

    読み込んだデータの参照が切れてしまう不具合を修正

--- ChainerModelLoader/ChainerModelDataLoader.cs
+++ ChainerModelLoader/ChainerModelDataLoader.cs
@@ -1,4 +1,5 @@
-﻿using KelpNet.Common;
+﻿using System;
+using KelpNet.Common;
 using KelpNet.Common.Functions;
 using KelpNet.Functions.Connections;
 using KelpNet.Functions.Normalization;
@@ -21,29 +22,29 @@ namespace ChainerModelLoader
         {
             if (func is Linear)
             {
-                ((Linear)func).W.Data = Real.GetArray(modelData[func.Name + "/W.npy"]);
-                ((Linear)func).b.Data = Real.GetArray(modelData[func.Name + "/b.npy"]);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/W.npy"]), ((Linear)func).W.Data, ((Linear)func).W.Data.Length);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/b.npy"]), ((Linear)func).b.Data, ((Linear)func).b.Data.Length);
             }
             else if (func is Convolution2D)
             {
-                ((Convolution2D)func).W.Data = Real.GetArray(modelData[func.Name + "/W.npy"]);
-                ((Convolution2D)func).b.Data = Real.GetArray(modelData[func.Name + "/b.npy"]);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/W.npy"]), ((Convolution2D)func).W.Data, ((Convolution2D)func).W.Data.Length);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/b.npy"]), ((Convolution2D)func).b.Data, ((Convolution2D)func).b.Data.Length);
             }
             else if (func is Deconvolution2D)
             {
-                ((Deconvolution2D)func).W.Data = Real.GetArray(modelData[func.Name + "/W.npy"]);
-                ((Deconvolution2D)func).b.Data = Real.GetArray(modelData[func.Name + "/b.npy"]);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/W.npy"]), ((Deconvolution2D)func).W.Data, ((Deconvolution2D)func).W.Data.Length);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/b.npy"]), ((Deconvolution2D)func).b.Data, ((Deconvolution2D)func).b.Data.Length);
             }
             else if (func is EmbedID)
             {
-                ((EmbedID) func).W.Data = Real.GetArray(modelData[func.Name + "/W.npy"]);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/W.npy"]), ((EmbedID)func).W.Data, ((EmbedID)func).W.Data.Length);
             }
             else if (func is BatchNormalization)
             {
-                ((BatchNormalization)func).Beta.Data = Real.GetArray(modelData[func.Name + "/beta.npy"]);
-                ((BatchNormalization)func).Gamma.Data = Real.GetArray(modelData[func.Name + "/gamma.npy"]);
-                ((BatchNormalization)func).AvgMean.Data = Real.GetArray(modelData[func.Name + "/avg_mean.npy"]);
-                ((BatchNormalization)func).AvgVar.Data = Real.GetArray(modelData[func.Name + "/avg_var.npy"]);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/beta.npy"]), ((BatchNormalization)func).Beta.Data, ((BatchNormalization)func).Beta.Data.Length);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/gamma.npy"]), ((BatchNormalization)func).Gamma.Data, ((BatchNormalization)func).Gamma.Data.Length);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/avg_mean.npy"]), ((BatchNormalization)func).AvgMean.Data, ((BatchNormalization)func).AvgMean.Data.Length);
+                Array.Copy(Real.GetArray(modelData[func.Name + "/avg_var.npy"]), ((BatchNormalization)func).AvgVar.Data, ((BatchNormalization)func).AvgVar.Data.Length);
             }
         }
     }
