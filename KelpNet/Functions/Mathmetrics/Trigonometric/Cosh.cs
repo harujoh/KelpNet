04c31ac1e22d7713a36eb2bd1be91591b0957d0e
harujoh <harujoh.yuki@gmail.com>
2017-10-20 16:15:23 +0900

    Cosh,Sinh,Tanhを追加

---
+++ KelpNet/Functions/Mathmetrics/Trigonometric/Cosh.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using KelpNet.Common;
+using KelpNet.Common.Functions;
+
+namespace KelpNet.Functions.Mathmetrics.Trigonometric
+{
+    public class Cosh : SingleInputFunction
+    {
+        private const string FUNCTION_NAME = "Cosh";
+
+        public Cosh(string name = FUNCTION_NAME) : base(name)
+        {
+            SingleInputForward = ForwardCpu;
+            SingleOutputBackward = BackwardCpu;
+        }
+
+        protected NdArray ForwardCpu(NdArray x)
+        {
+            Real[] resultData = new Real[x.Data.Length];
+
+            for (int i = 0; i < resultData.Length; i++)
+            {
+                resultData[i] = (Real)Math.Cosh(x.Data[i]);
+            }
+
+            return new NdArray(resultData, this);
+        }
+
+        protected void BackwardCpu(NdArray y, NdArray x)
+        {
+            for (int i = 0; i < y.Grad.Length; i++)
+            {
+                x.Grad[i] += (Real)Math.Sinh(x.Data[i]) * y.Grad[i];
+            }
+        }
+    }
+}
---
+++ KelpNet/Functions/Mathmetrics/Trigonometric/Tanh.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using KelpNet.Common;
+using KelpNet.Common.Functions;
+
+namespace KelpNet.Functions.Mathmetrics.Trigonometric
+{
+    public class Tanh : SingleInputFunction
+    {
+        private const string FUNCTION_NAME = "Tanh";
+
+        public Tanh(string name = FUNCTION_NAME) : base(name)
+        {
+            SingleInputForward = ForwardCpu;
+            SingleOutputBackward = BackwardCpu;
+        }
+
+        protected NdArray ForwardCpu(NdArray x)
+        {
+            Real[] resultData = new Real[x.Data.Length];
+
+            for (int i = 0; i < resultData.Length; i++)
+            {
+                resultData[i] = (Real)Math.Tanh(x.Data[i]);
+            }
+
+            return new NdArray(resultData, this);
+        }
+
+        protected void BackwardCpu(NdArray y, NdArray x)
+        {
+            for (int i = 0; i < y.Grad.Length; i++)
+            {
+                x.Grad[i] += y.Grad[i] * (1 - y.Data[i] * y.Data[i]);
+            }
+        }
+    }
+}
