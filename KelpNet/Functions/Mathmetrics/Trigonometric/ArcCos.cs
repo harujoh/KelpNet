63a5ed6984dca32a358d17381c8e40dc5fce4b32
harujoh <harujoh.yuki@gmail.com>
2017-10-20 16:02:01 +0900

    ArcSin,ArcCos,ArcTanを追加

---
+++ KelpNet/Functions/Mathmetrics/Trigonometric/ArcCos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using KelpNet.Common;
+using KelpNet.Common.Functions;
+
+namespace KelpNet.Functions.Mathmetrics.Trigonometric
+{
+    public class ArcCos : SingleInputFunction
+    {
+        private const string FUNCTION_NAME = "ArcCos";
+
+        public ArcCos(string name = FUNCTION_NAME) : base(name)
+        {
+            SingleInputForward = ForwardCpu;
+            SingleOutputBackward = BackwardCpu;
+        }
+
+        protected NdArray ForwardCpu(NdArray x)
+        {
+            Real[] resultData = new Real[x.Data.Length];
+
+            for (int i = 0; i < resultData.Length; i++)
+            {
+                resultData[i] = (Real)Math.Acos(x.Data[i]);
+            }
+
+            return new NdArray(resultData, this);
+        }
+
+        protected void BackwardCpu(NdArray y, NdArray x)
+        {
+            for (int i = 0; i < y.Grad.Length; i++)
+            {
+                x.Grad[i] += -1 / (Real)Math.Sqrt(-x.Data[i] * x.Data[i] + 1) * y.Grad[i];
+            }
+        }
+    }
+}
