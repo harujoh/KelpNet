0ecb79877add22567d58007c3e9a84fcf07f7e65
harujoh <harujoh.yuki@gmail.com>
2017-11-17 19:35:56 +0900

    バグ修正

--- CIFARLoader/Tar.cs
+++ CIFARLoader/Tar.cs
@@ -13,31 +13,22 @@ namespace CIFARLoader
             Dictionary<string, byte[]> result = new Dictionary<string, byte[]>();
 
             byte[] block = new byte[512];
-
             RawSerializer<HeaderBlock> serializer = new RawSerializer<HeaderBlock>();
+            int blocksToMunch = 0;
+            int remainingBytes = 0;
+
+            bool outputFlg = false;
+            string name = String.Empty;
+
+            List<byte> output = new List<byte>();
 
             using (Stream fs = new GZipStream(File.Open(archive, FileMode.Open, FileAccess.Read), CompressionMode.Decompress, false))
             {
                 while (fs.Read(block, 0, block.Length) > 0)
                 {
-                    HeaderBlock hb = serializer.RawDeserialize(block);
-
-                    var remainingBytes = hb.GetSize();
-
-                    if (hb.typeflag == 0)
+                    if (blocksToMunch > 0)
                     {
-                        hb.typeflag = (byte)'0';
-                    }
-
-                    var blocksToMunch = remainingBytes > 0 ? (remainingBytes - 1) / 512 + 1 : 0;
-
-                    if ((TarEntryType)hb.typeflag == TarEntryType.File_Old ||
-                        (TarEntryType)hb.typeflag == TarEntryType.File ||
-                        (TarEntryType)hb.typeflag == TarEntryType.File_Contiguous)
-                    {
-                        List<byte> output = new List<byte>();
-
-                        while (blocksToMunch > 0)
+                        if (outputFlg)
                         {
                             int bytesToWrite = block.Length < remainingBytes ? block.Length : remainingBytes;
 
@@ -47,13 +38,47 @@ namespace CIFARLoader
                             output.AddRange(tmpArray);
 
                             remainingBytes -= bytesToWrite;
-
-                            blocksToMunch--;
-
-                            if (fs.Read(block, 0, block.Length) > 0) break;
                         }
 
-                        result.Add(hb.GetName(), output.ToArray());
+                        blocksToMunch--;
+
+                        if (blocksToMunch == 0)
+                        {
+                            outputFlg = false;
+                        }
+
+                        continue;
+                    }
+
+                    if (name != string.Empty)
+                    {
+                        result.Add(name, output.ToArray());
+                        output.Clear();
+                    }
+
+                    HeaderBlock hb = serializer.RawDeserialize(block);
+
+
+                    if (name == String.Empty || (TarEntryType)hb.typeflag != TarEntryType.GnuLongName)
+                    {
+                        name = hb.GetName();
+                    }
+
+                    if (name == String.Empty) break;
+                    remainingBytes = hb.GetSize();
+
+                    if (hb.typeflag == 0)
+                    {
+                        hb.typeflag = (byte)'0';
+                    }
+
+                    blocksToMunch = remainingBytes > 0 ? (remainingBytes - 1) / 512 + 1 : 0;
+
+                    if ((TarEntryType)hb.typeflag == TarEntryType.File_Old ||
+                        (TarEntryType)hb.typeflag == TarEntryType.File ||
+                        (TarEntryType)hb.typeflag == TarEntryType.File_Contiguous)
+                    {
+                        outputFlg = true;
                     }
                 }
             }
