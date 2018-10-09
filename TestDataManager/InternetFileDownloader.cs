96e1f7a1634941ccdf41a8abd339ee0417d26283
harujoh <harujoh.yuki@gmail.com>
2018-10-09 19:43:18 +0900

    ダウンロードしたファイルのハッシュ値をチェックする機能を追加

--- TestDataManager/InternetFileDownloader.cs
+++ TestDataManager/InternetFileDownloader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Security.Cryptography;
 
 namespace TestDataManager
 {
@@ -9,30 +10,47 @@ namespace TestDataManager
         private const string TMP_DATA_PATH = "KelpNet/TestData/";
         static readonly string TmpFolderPath = Path.Combine(Path.GetTempPath(), TMP_DATA_PATH);
 
-        public static string Donwload(string url, string fileName)
+        public static string Donwload(string url, string fileName, string hash = "")
         {
             WebClient downloadClient = new WebClient();
 
             string savedPath = Path.Combine(TmpFolderPath, fileName);
 
-            if (File.Exists(fileName))
+            if (File.Exists(savedPath))
             {
-                return fileName;
+                //ファイルを開く
+                using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    //ハッシュ値を計算する
+                    byte[] bs = new MD5CryptoServiceProvider().ComputeHash(fs);
+
+                    string result = BitConverter.ToString(bs).ToLower().Replace("-", "");
+
+                    //読み込み成功
+                    if (hash == "" || result == hash)
+                    {
+                        return savedPath;
+                    }
+                }
+
+                //前回ダウンロードに失敗している
+                Console.WriteLine(fileName + "が破損しているため再ダウンロードを行います");
+
+                //失敗していたファイルを削除
+                File.Delete(fileName);
             }
 
             //ファイルのチェックとダウンロード
-            if (!File.Exists(savedPath))
+            Console.WriteLine(fileName + "をダウンロードします");
+
+            //テンポラリにフォルダがなければ作成する
+            if (!Directory.Exists(TmpFolderPath))
             {
-                Console.WriteLine(fileName + "をダウンロードします");
-
-                if (!Directory.Exists(TmpFolderPath))
-                {
-                    Directory.CreateDirectory(TmpFolderPath);
-                }
-
-                //非同期ダウンロードを開始する
-                downloadClient.DownloadFileTaskAsync(new Uri(url), savedPath).Wait();
+                Directory.CreateDirectory(TmpFolderPath);
             }
+
+            //非同期ダウンロードを開始する
+            downloadClient.DownloadFileTaskAsync(new Uri(url), savedPath).Wait();
 
             return savedPath;
         }
