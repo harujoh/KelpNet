e6c9856f3738fdf20dda61cb45de5906911185b5
harujoh <harujoh.yuki@gmail.com>
2020-05-06 09:57:10 +0900

    LSTMのIndex操作を見直した Paramの保管と破棄のロジックをまとめた

--- KelpNet.Function/Functions/Connections/LSTM.cs
+++ KelpNet.Function/Functions/Connections/LSTM.cs
@@ -29,33 +29,21 @@ namespace KelpNet
 
         public Linear<T> lateral;
 
-        private List<T[]> aParam = new List<T[]>();
-        private List<T[]> iParam = new List<T[]>();
-        private List<T[]> fParam = new List<T[]>();
-        private List<T[]> oParam = new List<T[]>();
-        public List<T[]> cNextParam = new List<T[]>();
-        private List<T[]> cPrevParam = new List<T[]>();
-        private List<NdArray<T>> hPrevParams = new List<NdArray<T>>();
+        private List<T[][]> paramLists = new List<T[][]>(); //0:cPrev 1:a 2:i 3:f 4:o 5:c
+        private List<T[][]> usedParamLists = new List<T[][]>();
 
-        private List<T[]> aUsedParam = new List<T[]>();
-        private List<T[]> iUsedParam = new List<T[]>();
-        private List<T[]> fUsedParam =new List<T[]>();
-        private List<T[]> oUsedParam = new List<T[]>();
-        public List<T[]> cUsedNextParam = new List<T[]>();
-        private List<T[]> cUsedPrevParam = new List<T[]>();
+        private List<NdArray<T>> hPrevParams = new List<NdArray<T>>();
         private List<NdArray<T>> hUsedPrevParams = new List<NdArray<T>>();
 
         private List<T[]> gxPrevGrads = new List<T[]>();
 
         public NdArray<T> hParam;
-        private T[] cPrev = {};
+        private T[] cPrev = { };
 
-        public readonly int InputCount;
         public readonly int OutputCount;
 
         public LSTM(int inSize, int outSize, Array lateralInit = null, Array upwardInit = null, Array biasInit = null, Array forgetBiasInit = null, string name = FUNCTION_NAME, string[] inputNames = null, string[] outputNames = null, bool gpuEnable = false) : base(name, inputNames, outputNames)
         {
-            this.InputCount = inSize;
             this.OutputCount = outSize;
 
             List<NdArray<T>> functionParameters = new List<NdArray<T>>();
@@ -123,13 +111,13 @@ namespace KelpNet
             switch (this)
             {
                 case LSTM<float> lstmF:
-                    lstmF.SingleInputForward = (x) => LSTMF.SingleInputForward(x, lstmF.upward, lstmF.lateral, lstmF.aParam, lstmF.iParam, lstmF.fParam, lstmF.oParam, lstmF.cNextParam, lstmF.cPrevParam, lstmF.hPrevParams, ref lstmF.hParam,ref lstmF.cPrev, lstmF.OutputCount, lstmF);
-                    lstmF.SingleOutputBackward = (y, x) => LSTMF.SingleOutputBackward(y, lstmF.upward, lstmF.lateral, lstmF.aParam, lstmF.iParam, lstmF.fParam, lstmF.oParam, lstmF.cNextParam, lstmF.cPrevParam, lstmF.hPrevParams, lstmF.aUsedParam, lstmF.iUsedParam, lstmF.fUsedParam, lstmF.oUsedParam, lstmF.cUsedNextParam, lstmF.cUsedPrevParam, lstmF.hUsedPrevParams, lstmF.gxPrevGrads, lstmF.OutputCount, lstmF.Backward);
+                    lstmF.SingleInputForward = (x) => LSTMF.SingleInputForward(x, lstmF.upward, lstmF.lateral, lstmF.paramLists, lstmF.hPrevParams, ref lstmF.hParam, ref lstmF.cPrev, lstmF.OutputCount, lstmF);
+                    lstmF.SingleOutputBackward = (y, x) => LSTMF.SingleOutputBackward(y, lstmF.upward, lstmF.lateral, lstmF.paramLists, lstmF.hPrevParams, lstmF.usedParamLists, lstmF.hUsedPrevParams, lstmF.gxPrevGrads, lstmF.OutputCount, lstmF.Backward);
                     break;
 
                 case LSTM<double> lstmD:
-                    lstmD.SingleInputForward = (x) => LSTMD.SingleInputForward(x, lstmD.upward, lstmD.lateral, lstmD.aParam, lstmD.iParam, lstmD.fParam, lstmD.oParam, lstmD.cNextParam, lstmD.cPrevParam, lstmD.hPrevParams, ref lstmD.hParam,ref lstmD.cPrev, lstmD.OutputCount, lstmD);
-                    lstmD.SingleOutputBackward = (y, x) => LSTMD.SingleOutputBackward(y, lstmD.upward, lstmD.lateral, lstmD.aParam, lstmD.iParam, lstmD.fParam, lstmD.oParam, lstmD.cNextParam, lstmD.cPrevParam, lstmD.hPrevParams, lstmD.aUsedParam, lstmD.iUsedParam, lstmD.fUsedParam, lstmD.oUsedParam, lstmD.cUsedNextParam, lstmD.cUsedPrevParam, lstmD.hUsedPrevParams, lstmD.gxPrevGrads, lstmD.OutputCount, lstmD.Backward);
+                    lstmD.SingleInputForward = (x) => LSTMD.SingleInputForward(x, lstmD.upward, lstmD.lateral, lstmD.paramLists, lstmD.hPrevParams, ref lstmD.hParam, ref lstmD.cPrev, lstmD.OutputCount, lstmD);
+                    lstmD.SingleOutputBackward = (y, x) => LSTMD.SingleOutputBackward(y, lstmD.upward, lstmD.lateral, lstmD.paramLists, lstmD.hPrevParams, lstmD.usedParamLists, lstmD.hUsedPrevParams, lstmD.gxPrevGrads, lstmD.OutputCount, lstmD.Backward);
                     break;
             }
         }
@@ -139,20 +127,10 @@ namespace KelpNet
             base.ResetState();
             this.hParam = null;
 
-            this.aParam = new List<T[]>();
-            this.iParam = new List<T[]>();
-            this.fParam = new List<T[]>();
-            this.oParam = new List<T[]>();
-            this.cNextParam = new List<T[]>();
-            this.cPrevParam = new List<T[]>();
+            this.paramLists = new List<T[][]>();
             this.hPrevParams = new List<NdArray<T>>();
 
-            this.aUsedParam = new List<T[]>();
-            this.iUsedParam = new List<T[]>();
-            this.fUsedParam = new List<T[]>();
-            this.oUsedParam = new List<T[]>();
-            this.cUsedNextParam = new List<T[]>();
-            this.cUsedPrevParam = new List<T[]>();
+            this.usedParamLists = new List<T[][]>();
             this.hUsedPrevParams = new List<NdArray<T>>();
 
             this.gxPrevGrads = new List<T[]>();
@@ -166,15 +144,15 @@ namespace KelpNet
     public static class LSTMF
 #endif
     {
-        public static NdArray<Real> SingleInputForward(NdArray<Real> x, IFunction<Real> upward, IFunction<Real> lateral, List<Real[]> aParam, List<Real[]> iParam, List<Real[]> fParam, List<Real[]> oParam, List<Real[]> cNextParam, List<Real[]> cPrevParam, List<NdArray<Real>> hPrevParams, ref NdArray<Real> hParam,ref Real[] cPrev, int outputCount, IFunction<Real> lstm)
+        public static NdArray<Real> SingleInputForward(NdArray<Real> x, IFunction<Real> upward, IFunction<Real> lateral, List<Real[][]> paramList, List<NdArray<Real>> hPrevParams, ref NdArray<Real> hParam, ref Real[] lcPrev, int outputCount, IFunction<Real> lstm)
         {
-            NdArray<Real> lstmIn = upward.Forward(x)[0];
-
             int outputDataSize = x.BatchCount * outputCount;
+
+            NdArray<Real> lstmIn = upward.Forward(x)[0];
 
             if (hParam == null)
             {
-                cPrev = new Real[outputDataSize];
+                lcPrev = new Real[outputDataSize];
             }
             else
             {
@@ -184,102 +162,62 @@ namespace KelpNet
                 hPrevParams.Add(hPrevParam);
             }
 
-            Real[] la = new Real[outputDataSize];
-            Real[] li = new Real[outputDataSize];
-            Real[] lf = new Real[outputDataSize];
-            Real[] lo = new Real[outputDataSize];
-            Real[] cNext = new Real[outputDataSize];
+            //0:cPrev 1:a 2:i 3:f 4:o 5:c
+            Real[][] param = { lcPrev, new Real[outputDataSize], new Real[outputDataSize], new Real[outputDataSize], new Real[outputDataSize], new Real[outputDataSize] };
             Real[] lhParam = new Real[outputDataSize];
 
-            for (int b = 0; b < x.BatchCount; b++)
+            int index = 0;
+            for (int outIndex = 0; outIndex < lhParam.Length; outIndex++)
             {
-                int index = b * lstmIn.Length;
+                param[1][outIndex] = KelpMath.Tanh(lstmIn.Data[index++]);
+                param[2][outIndex] = Sigmoid(lstmIn.Data[index++]);
+                param[3][outIndex] = Sigmoid(lstmIn.Data[index++]);
+                param[4][outIndex] = Sigmoid(lstmIn.Data[index++]);
 
-                for (int i = 0; i < outputCount; i++)
-                {
-                    int outIndex = b * outputCount + i;
+                param[5][outIndex] = param[1][outIndex] * param[2][outIndex] + param[3][outIndex] * param[0][outIndex];
 
-                    la[outIndex] = KelpMath.Tanh(lstmIn.Data[index++]);
-                    li[outIndex] = Sigmoid(lstmIn.Data[index++]);
-                    lf[outIndex] = Sigmoid(lstmIn.Data[index++]);
-                    lo[outIndex] = Sigmoid(lstmIn.Data[index++]);
-
-                    cNext[outIndex] = la[outIndex] * li[outIndex] + lf[outIndex] * cPrev[outIndex];
-
-                    lhParam[outIndex] = lo[outIndex] * KelpMath.Tanh(cNext[outIndex]);
-                }
+                lhParam[outIndex] = param[4][outIndex] * KelpMath.Tanh(param[5][outIndex]);
             }
 
-            cPrevParam.Add(cPrev);
-            cNextParam.Add(cNext);
-            aParam.Add(la);
-            iParam.Add(li);
-            fParam.Add(lf);
-            oParam.Add(lo);
+            paramList.Add(param);
 
             //Backwardで消えないように別で保管
-            cPrev = cNext;
+            lcPrev = param[5];
 
             hParam = new NdArray<Real>(lhParam, new[] { outputCount }, x.BatchCount, lstm);
             return hParam;
         }
 
-        public static void SingleOutputBackward(NdArray<Real> y, IFunction<Real> upward, IFunction<Real> lateral, List<Real[]> aParam, List<Real[]> iParam, List<Real[]> fParam, List<Real[]> oParam, List<Real[]> cNextParam, List<Real[]> cPrevParam, List<NdArray<Real>> hPrevParams, List<Real[]> aUsedParam, List<Real[]> iUsedParam, List<Real[]> fUsedParam, List<Real[]> oUsedParam, List<Real[]> cUsedNextParam, List<Real[]> cUsedPrevParam, List<NdArray<Real>> hUsedPrevParams, List<Real[]> gxPrevGrads, int outputCount, ActionOptional<Real> backward)
+        public static void SingleOutputBackward(NdArray<Real> y, IFunction<Real> upward, IFunction<Real> lateral, List<Real[][]> paramLists, List<NdArray<Real>> hPrevParams, List<Real[][]> usedParamLists, List<NdArray<Real>> hUsedPrevParams, List<Real[]> gxPrevGrads, int outputCount, ActionOptional<Real> backward)
         {
             Real[] gxPrevGrad = new Real[y.BatchCount * outputCount * 4];
-
             Real[] gcPrev = new Real[y.BatchCount * outputCount];
 
-            Real[] lcNextParam = cNextParam[cPrevParam.Count - 1];
-            cNextParam.RemoveAt(cNextParam.Count - 1);
-            cUsedNextParam.Add(lcNextParam);
+            //0:cPrev 1:a 2:i 3:f 4:o 5:c
+            Real[][] param = paramLists[paramLists.Count - 1];
+            paramLists.RemoveAt(paramLists.Count - 1);
+            usedParamLists.Add(param);
 
-            Real[] tanh_a = aParam[aParam.Count - 1];
-            aParam.RemoveAt(aParam.Count - 1);
-            aUsedParam.Add(tanh_a);
-
-            Real[] sig_i = iParam[iParam.Count - 1];
-            iParam.RemoveAt(iParam.Count - 1);
-            iUsedParam.Add(sig_i);
-
-            Real[] sig_f = fParam[fParam.Count - 1];
-            fParam.RemoveAt(fParam.Count - 1);
-            fUsedParam.Add(sig_f);
-
-            Real[] sig_o = oParam[oParam.Count - 1];
-            oParam.RemoveAt(oParam.Count - 1);
-            oUsedParam.Add(sig_o);
-
-            Real[] lcPrev = cPrevParam[cPrevParam.Count - 1];
-            cPrevParam.RemoveAt(cPrevParam.Count - 1);
-            cUsedPrevParam.Add(lcPrev);
-
-            for (int b = 0; b < y.BatchCount; b++)
+            int index = 0;
+            for (int prevOutputIndex = 0; prevOutputIndex < gcPrev.Length; prevOutputIndex++)
             {
-                int index = b * outputCount * 4;
+                Real co = KelpMath.Tanh(param[5][prevOutputIndex]);
 
-                for (int i = 0; i < outputCount; i++)
-                {
-                    int prevOutputIndex = b * outputCount + i;
+                gcPrev[prevOutputIndex] += y.Grad[prevOutputIndex] * param[4][prevOutputIndex] * GradTanh(co);
 
-                    Real co = KelpMath.Tanh(lcNextParam[prevOutputIndex]);
+                gxPrevGrad[index++] = gcPrev[prevOutputIndex] * param[2][prevOutputIndex] * GradTanh(param[1][prevOutputIndex]);
+                gxPrevGrad[index++] = gcPrev[prevOutputIndex] * param[1][prevOutputIndex] * GradSigmoid(param[2][prevOutputIndex]);
+                gxPrevGrad[index++] = gcPrev[prevOutputIndex] * param[0][prevOutputIndex] * GradSigmoid(param[3][prevOutputIndex]);
+                gxPrevGrad[index++] = y.Grad[prevOutputIndex] * co * GradSigmoid(param[4][prevOutputIndex]);
 
-                    gcPrev[prevOutputIndex] += y.Grad[prevOutputIndex] * sig_o[prevOutputIndex] * GradTanh(co);
-
-                    gxPrevGrad[index++] = gcPrev[prevOutputIndex] * sig_i[prevOutputIndex] * GradTanh(tanh_a[prevOutputIndex]);
-                    gxPrevGrad[index++] = gcPrev[prevOutputIndex] * tanh_a[prevOutputIndex] * GradSigmoid(sig_i[prevOutputIndex]);
-                    gxPrevGrad[index++] = gcPrev[prevOutputIndex] * lcPrev[prevOutputIndex] * GradSigmoid(sig_f[prevOutputIndex]);
-                    gxPrevGrad[index++] = y.Grad[prevOutputIndex] * co * GradSigmoid(sig_o[prevOutputIndex]);
-
-                    gcPrev[prevOutputIndex] *= sig_f[prevOutputIndex];
-                }
+                gcPrev[prevOutputIndex] *= param[3][prevOutputIndex];
             }
 
             gxPrevGrads.Add(gxPrevGrad);
 
-            //gxPrevをlateralとupwardに渡すことでaddのBackwardを兼ねる
             if (hPrevParams.Count > 0)
             {
+                //linearのBackwardはgxPrev.Gradしか使わないのでgxPrev.Dataは空
                 NdArray<Real> gxPrev = new NdArray<Real>(new[] { outputCount * 4 }, y.BatchCount);
                 gxPrev.Grad = gxPrevGrad;
                 lateral.Backward(gxPrev);
@@ -299,27 +237,17 @@ namespace KelpNet
                 }
             }
 
+            //linearのBackwardはgy.Gradしか使わないのでgy.Dataは空
             NdArray<Real> gy = new NdArray<Real>(new[] { outputCount * 4 }, y.BatchCount);
             gy.Grad = gxPrevGrads[0];
             gxPrevGrads.RemoveAt(0);
             upward.Backward(gy);
 
-            //linearのBackwardはgxPrev.Gradしか使わないのでgxPrev.Dataは空
             //使い切ったら戻す
-            if (cNextParam.Count == 0)
+            if (paramLists.Count == 0)
             {
-                cNextParam.AddRange(cUsedNextParam);
-                aParam.AddRange(aUsedParam);
-                iParam.AddRange(iUsedParam);
-                fParam.AddRange(fUsedParam);
-                oParam.AddRange(oUsedParam);
-                cPrevParam.AddRange(cUsedPrevParam);
-                cUsedNextParam.Clear();
-                aUsedParam.Clear();
-                iUsedParam.Clear();
-                fUsedParam.Clear();
-                oUsedParam.Clear();
-                cUsedPrevParam.Clear();
+                paramLists.AddRange(usedParamLists);
+                usedParamLists.Clear();
             }
         }
 
