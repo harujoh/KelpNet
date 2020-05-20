c4522ef6cded9c734ca4570a05be093ad0e60a9c
harujoh <harujoh.yuki@gmail.com>
2020-05-20 17:31:02 +0900

    ONNX対応 ONNXのVGGを起動するSample20を追加

---
+++ KelpNet.Sample/README.md
@@ -0,0 +1,62 @@
+## Sample01
+MLPによるXORの学習
+
+## Sample02
+MLPによるXORの学習【回帰版】
+
+## Sample03
+MLPによるSin関数の学習
+
+## Sample04
+MLPによるMNIST（手書き文字）の学習
+
+## Sample05
+エクセルCNNの再現
+
+## Sample06
+5層CNNによるMNISTの学習
+
+## Sample07
+BatchNormを使った15層MLPによるMNISTの学習
+
+## Sample08
+LSTMによるSin関数の学習
+
+## Sample09
+SimpleなRNNによるRNNLM
+
+## Sample10
+LSTMによるRNNLM
+
+## Sample11
+Decoupled Neural Interfaces using Synthetic GradientsによるMNISTの学習
+
+## Sample12
+Test11のDNIをcDNIとした
+
+## Sample13
+Deconvolution2Dのテスト(Winform)
+
+## Sample14
+Test6を連結して実行
+
+## Sample15
+CaffeモデルのVGGを読み込んで画像分類をさせるテスト
+
+## Sample16
+ChainerモデルのTest5と同じ内容を読み込んで実行
+
+## Sample17
+CaffeモデルのRESNETを読み込んで画像分類をさせるテスト
+
+## Sample18
+CIFAR-10を5層CNNを使って学習する
+
+## Sample19
+CaffeモデルのAlexNetを読み込んで画像分類をさせるテスト
+
+## Sample20
+ONNXモデルのVGGを読み込んで画像分類をさせるテスト
+
+## SampleX
+Linearの分割実行
---
+++ KelpNet.Sample/Sample/Sample20.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using KelpNet.CL;
+using KelpNet.Tools;
+
+//using Real = System.Double;
+using Real = System.Single;
+
+namespace KelpNet.Sample
+{
+    class Sample20
+    {
+        private const string DOWNLOAD_URL = "https://github.com/onnx/models/blob/master/vision/classification/vgg/model/";
+        private const string VGG16_MODEL_FILE = "vgg16-7.onnx";
+        private const string VGG19_MODEL_FILE = "vgg19-7.onnx";
+        private const string VGG16_MODEL_FILE_HASH = "a5a3fd73d345152852568509ebff19fc";
+        private const string VGG19_MODEL_FILE_HASH = "";
+        private const string CLASS_LIST_PATH = "Data/synset_words.txt";
+
+        private static readonly string[] Urls = { DOWNLOAD_URL + VGG16_MODEL_FILE, DOWNLOAD_URL + VGG19_MODEL_FILE };
+        private static readonly string[] FileNames = { VGG16_MODEL_FILE, VGG19_MODEL_FILE };
+        private static readonly string[] Hashes = { VGG16_MODEL_FILE_HASH, VGG19_MODEL_FILE_HASH };
+
+        public enum VGGModel
+        {
+            VGG16,
+            VGG19
+        }
+
+        public static void Run(VGGModel modelType)
+        {
+            OpenFileDialog ofd = new OpenFileDialog { Filter = "画像ファイル(*.jpg;*.png;*.gif;*.bmp)|*.jpg;*.png;*.gif;*.bmp|すべてのファイル(*.*)|*.*" };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                int vggId = (int)modelType;
+
+                Console.WriteLine("Model Loading.");
+                string modelFilePath = InternetFileDownloader.Donwload(Urls[vggId], FileNames[vggId], Hashes[vggId]);
+
+                List<Function<Real>> vggNet = OnnxmodelDataLoader.LoadNetWork<Real>(modelFilePath);
+                while (vggNet.Remove(null)) { }
+
+                string[] classList = File.ReadAllLines(CLASS_LIST_PATH);
+
+                //GPUを初期化
+                for (int i = 0; i < vggNet.Count - 1; i++)
+                {
+                    if (vggNet[i] is CPU.Convolution2D<Real> || vggNet[i] is CPU.Linear<Real> || vggNet[i] is CPU.MaxPooling2D<Real>)
+                    {
+                        vggNet[i] = (Function<Real>)CLConverter.Convert(vggNet[i]);
+                    }
+                }
+
+                FunctionStack<Real> nn = new FunctionStack<Real>(vggNet.ToArray());
+
+                //層を圧縮
+                nn.Compress();
+
+                Console.WriteLine("Model Loading done.");
+
+                do
+                {
+                    //ネットワークへ入力する前に解像度を 224px x 224px x 3ch にしておく
+                    Bitmap baseImage = new Bitmap(ofd.FileName);
+                    Bitmap resultImage = new Bitmap(224, 224, PixelFormat.Format24bppRgb);
+                    Graphics g = Graphics.FromImage(resultImage);
+                    g.DrawImage(baseImage, 0, 0, 224, 224);
+                    g.Dispose();
+
+                    Real[] bias = new Real[] { -123.68f, -116.779f, -103.939f }; //補正値のチャンネル順は入力画像に従う(標準的なBitmapならRGB)
+                    NdArray<Real> imageArray = BitmapConverter.Image2NdArray<Real>(resultImage, false, true, bias);
+
+                    Console.WriteLine("Start predict.");
+                    Stopwatch sw = Stopwatch.StartNew();
+                    NdArray<Real> result = nn.Predict(imageArray)[0];
+                    sw.Stop();
+
+                    Console.WriteLine("Result Time : " + (sw.ElapsedTicks / (Stopwatch.Frequency / (1000L * 1000L))).ToString("n0") + "μｓ");
+
+                    int maxIndex = Array.IndexOf(result.Data, result.Data.Max());
+
+                    Console.WriteLine("[" + result.Data[maxIndex] + "] : " + classList[maxIndex]);
+
+                } while (ofd.ShowDialog() == DialogResult.OK);
+            }
+        }
+
+    }
+}
