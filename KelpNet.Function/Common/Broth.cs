70cdc126131dd5ee75f6b0540965ea541854238b
harujoh <harujoh.yuki@gmail.com>
2020-07-24 16:39:30 +0900

    Broth refactoring.

--- KelpNet.Function/Common/Broth.cs
+++ KelpNet.Function/Common/Broth.cs
@@ -14,13 +14,11 @@ namespace KelpNet
     //外部公開を必要としないinternalなクラスなので、それぞれの型で名称を変えずに済む
     internal class Broth
     {
-        // 平均mu, 標準偏差sigmaの正規分布乱数を得る。Box-Muller法による。
+        // 平均mu, 標準偏差sigmaの正規分布乱数を得る
         public static Real RandomNormal(Real sigma = 1, Real mu = 0)
         {
-            Real _beta = Broth.Random() * Math.PI * 2;
-            Real _boxMuller1 = Math.Sqrt(-2 * Math.Log(Broth.Random()));
-
-            return sigma * _boxMuller1 * Math.Sin(_beta) + mu;
+            Real boxMuller = Math.Sqrt(-2 * Math.Log(Broth.Random())) * Math.Sin(2 * Math.PI * Broth.Random());
+            return sigma * boxMuller + mu;
         }
 
         public static Real Random()
