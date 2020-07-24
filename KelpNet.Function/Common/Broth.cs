5824b86b7c47d2d37b417e27e50fd62525795325
harujoh <harujoh.yuki@gmail.com>
2020-07-24 16:27:42 +0900

    Fixed the method of generating random numbers.

--- KelpNet.Function/Common/Broth.cs
+++ KelpNet.Function/Common/Broth.cs
@@ -14,29 +14,13 @@ namespace KelpNet
     //外部公開を必要としないinternalなクラスなので、それぞれの型で名称を変えずに済む
     internal class Broth
     {
-        static bool _flip;
-        static Real _beta;
-        static Real _boxMuller1;
-
         // 平均mu, 標準偏差sigmaの正規分布乱数を得る。Box-Muller法による。
         public static Real RandomNormal(Real sigma = 1, Real mu = 0)
         {
-            Real boxMuller2;
+            Real _beta = Broth.Random() * Math.PI * 2;
+            Real _boxMuller1 = Math.Sqrt(-2 * Math.Log(Broth.Random()));
 
-            if (!_flip)
-            {
-                _beta = Broth.Random() * Math.PI * 2;
-                _boxMuller1 = Math.Sqrt(-2 * Math.Log(Broth.Random()));
-                boxMuller2 = Math.Sin(_beta);
-            }
-            else
-            {
-                boxMuller2 = Math.Cos(_beta);
-            }
-
-            _flip = !_flip;
-
-            return sigma * _boxMuller1 * boxMuller2 + mu;
+            return sigma * _boxMuller1 * Math.Sin(_beta) + mu;
         }
 
         public static Real Random()
